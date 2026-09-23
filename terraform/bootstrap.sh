0ca641be88f35e3d089f5e0610b282b977ab2ca9
Datta Rahegaonkar <dattarahegaonkar09@gmail.com>
2026-09-23 09:26:54 +0000

    Added: Updated Infra and EKS cluster configuration

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+# ArgoCD Namespace Creation
 kubectl create namespace argocd
 
+# ArgoCD Helm Repo
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
+# ArgoCD Installation
 helm install argocd argo/argo-cd -n argocd
 
+# Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.15.1/deploy/static/provider/cloud/deploy.yaml
 
+# Install ArgoCD CLI
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
-
 argocd version --client
+
+# Install Image Upadate
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/v0.14.0/manifests/install.yaml
+
